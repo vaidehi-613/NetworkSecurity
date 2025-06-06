40cecb4ab55d9f94404fe60ae16359c700ff5d16
Vaidehi Pawar <vaidehipawar@Vaidehis-MacBook-Air.local>
2025-06-06 14:14:01 -0700

    Data Ingestion Component completed

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,6 +1,6 @@
 import sys
 from networksecurity.logging import logger
-class NetworkSecurtiyException(Exception):
+class NetworkSecurityException(Exception):
     def __init__(self, error_message, error_details:sys):
         self.error_message = error_message
         _,_,exc_tb = error_details.exc_info()
