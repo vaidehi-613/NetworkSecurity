6525d29a9bf22c2db47dcec0801d973ef128c575
Vaidehi Pawar <vaidehipawar@Vaidehis-MacBook-Air.local>
2025-06-19 16:28:55 -0700

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 import sys
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 
@@ -19,13 +20,19 @@ if __name__ == "__main__":
         print(dataingestionartifact)
 
         datavalidationconfig = DataValidationConfig(trainingpipelineconfig)
-
-        # ✅ FIX: Pass the artifact, not the config
         data_validation = DataValidation(dataingestionartifact, datavalidationconfig)
 
         logging.info("Initiate the data validation pipeline")
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
+        print(data_validation_artifact)
+
+        data_transformation_config  = DataTransformationConfig(trainingpipelineconfig)
+        logging.info("Data Transformation started")
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation Completed")
+        print(data_transformation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
